f0895e4e617b0a5a924a0c800d1a5dfdab712b63
Duzy Chan <duzy@duzy.info>
2010-09-07 23:52:46 +0800

    fixed: make clean

--- tools/common.mk
+++ tools/common.mk
@@ -1,9 +1,23 @@
 #
 
 define sm.tool.common.rm
-rm -f
+$(if $1,rm -f $1)
 endef #sm.tool.common.rm
 
 define sm.tool.common.rmdir
-rm -rf
+$(if $1,rm -rf $1)
 endef #sm.tool.common.rmdir
+
+define sm.tool.common.mkdir
+$(if $1,mkdir -p $1)
+endef #sm.tool.common.mkdir
+
+define sm.tool.common.cp
+$(if $(and $1,$2),cp $1 $2,\
+  $(error smart: copy command requires more than two args))
+endef #sm.tool.common.cp
+
+define sm.tool.common.mv
+$(if $(and $1,$2),mv $1 $2,\
+  $(error smart: move command requires more than two args))
+endef #sm.tool.common.mv
