b2716569072c0c19db94bc71c7ddc2de01c15684
Duzy Chan <duzy@duzy.info>
2010-11-16 18:48:21 +0800

    fix: sm.tool.gcc.cmd.as -> sm.tool.gcc.cmd.asm; working around clang toolset for building

--- tools/clang.mk
+++ tools/clang.mk
@@ -6,6 +6,40 @@
 ##  sm.toolset.clang
 ##
 
-sm.toolset.clang.compile.c++ := clang++
-sm.toolset.clang.compile.c := clang
+## make sure that clang.mk is included only once
+$(call sm-check-origin, sm.tool.clang, undefined)
 
+sm.tool.clang := true
+
+sm.tool.clang.cmd.cc := clang
+sm.tool.clang.cmd.c++ := clang++
+sm.tool.clang.cmd.asm := gas
+sm.tool.clang.cmd.ll := llvmc
+sm.tool.clang.cmd.ld := ld
+sm.tool.clang.cmd.ar := ar crs
+
+sm.tool.clang.langs := c c++ ll
+sm.tool.clang.c.suffix :=
+sm.tool.clang.c++.suffix :=
+sm.tool.clang.ll.suffix :=
+
+######################################################################
+# Compilation
+
+define sm.tool.clang.compile.c
+$(sm.tool.clang.cmd.cc) $($(strip $3)) -c -o $(strip $1) $(strip $2)
+endef #sm.tool.clang.compile.c
+
+define sm.tool.clang.compile.c++
+$(sm.tool.clang.cmd.c++) $($(strip $3)) -c -o $(strip $1) $(strip $2)
+endef #sm.tool.clang.compile.c++
+
+define sm.tool.clang.compile.asm
+$(sm.tool.clang.cmd.asm) $($(strip $3)) -c -o $(strip $1) $(strip $2)
+endef #sm.tool.clang.compile.asm
+
+define sm.tool.clang.compile.ll
+$(sm.tool.clang.cmd.ll) $($(strip $3)) -c -o $(strip $1) $(strip $2)
+endef #sm.tool.clang.compile.ll
+
+# TODO: ...
--- tools/gcc.mk
+++ tools/gcc.mk
@@ -6,13 +6,15 @@
 ##  sm.tool.gcc
 ##
 
+## make sure that gcc.mk is included only once
 $(call sm-check-origin, sm.tool.gcc, undefined)
 
 sm.tool.gcc := true
 
+## basic command names
 sm.tool.gcc.cmd.cc := gcc
 sm.tool.gcc.cmd.c++ := g++
-sm.tool.gcc.cmd.as := gas
+sm.tool.gcc.cmd.asm := gas
 sm.tool.gcc.cmd.ld := ld
 sm.tool.gcc.cmd.ar := ar crs
 
@@ -23,29 +25,29 @@ sm.tool.gcc.c.suffix :=
 sm.tool.gcc.c++.suffix :=
 sm.tool.gcc.asm.suffix :=
 
-##################################################
+######################################################################
 # Compiles
 
 ##
 ##  Produce compile commands for c language
 ##
-define sm.tool.gcc.compile.c.unchecked
+define sm.tool.gcc.compile.c.private
 $(sm.tool.gcc.cmd.cc) $($(strip $3)) -c -o $(strip $1) $(strip $2)
-endef #sm.tool.gcc.compile.c.unchecked
+endef #sm.tool.gcc.compile.c.private
 
 ##
 ##
 ##
-define sm.tool.gcc.compile.c++.unchecked
+define sm.tool.gcc.compile.c++.private
 $(sm.tool.gcc.cmd.c++) $($(strip $3)) -c -o $(strip $1) $(strip $2)
-endef #sm.tool.gcc.compile.c++.unchecked
+endef #sm.tool.gcc.compile.c++.private
 
 ##
 ##
 ##
-define sm.tool.gcc.compile.asm.unchecked
+define sm.tool.gcc.compile.asm.private
 $(sm.tool.gcc.cmd.asm) $($(strip $3)) -c -o $(strip $1) $(strip $2)
-endef #sm.tool.gcc.compile.asm.unchecked
+endef #sm.tool.gcc.compile.asm.private
 
 ##
 ##
@@ -55,7 +57,7 @@ $(if $1,,$(error smart: arg \#1 must be the source language))\
 $(if $2,,$(error smart: arg \#2 must be the output target))\
 $(if $3,,$(error smart: arg \#3 must be the source file))\
 $(if $4,,$(error smart: arg \#4 must be a callback for compile flags))\
-$(call sm.tool.gcc.compile.$(strip $1).unchecked,$2,$3,$(strip $4))
+$(call sm.tool.gcc.compile.$(strip $1).private,$2,$3,$(strip $4))
 endef #sm.tool.gcc.compile
 
 sm.tool.gcc.compile.c = $(call sm.tool.gcc.compile,c,$1,$2,$3)
@@ -66,13 +68,13 @@ sm.tool.gcc.compile.asm = $(call sm.tool.gcc.compile,asm,$1,$2,$3)
 ##################################################
 # Denpendencies
 
-define sm.tool.gcc.dependency.c.unchecked
+define sm.tool.gcc.dependency.c.private
 $(sm.tool.gcc.cmd.cc) -MM -MT $(strip $2) -MF $(strip $1) $($(strip $4)) $(strip $3)
-endef #sm.tool.gcc.dependency.c.unchecked
+endef #sm.tool.gcc.dependency.c.private
 
-define sm.tool.gcc.dependency.c++.unchecked
+define sm.tool.gcc.dependency.c++.private
 $(sm.tool.gcc.cmd.c++) -MM -MT $(strip $2) -MF $(strip $1) $($(strip $4)) $(strip $3)
-endef #sm.tool.gcc.dependency.c++.unchecked
+endef #sm.tool.gcc.dependency.c++.private
 
 ## eg. $(sm.tool.gcc.dependency, foo.d, foo.o, src/foo.cpp)
 define sm.tool.gcc.dependency
@@ -80,12 +82,13 @@ $(if $1,,$(error smart: arg \#1 must be the source language))\
 $(if $2,,$(error smart: arg \#2 must be the output target))\
 $(if $3,,$(error smart: arg \#3 must be the source file))\
 $(if $4,,$(error smart: arg \#4 must be a callback for compile flags))\
-$(call sm.tool.gcc.dependency.$(strip $1).unchecked,$2,$3,$(strip $4),$(strip $5))
+$(call sm.tool.gcc.dependency.$(strip $1).private,$2,$3,$(strip $4),$(strip $5))
 endef #sm.tool.gcc.dependency
 
 sm.tool.gcc.dependency.c = $(call sm.tool.gcc.dependency,c,$1,$2,$3,$4)
 sm.tool.gcc.dependency.c++ = $(call sm.tool.gcc.dependency,c++,$1,$2,$3,$4)
 sm.tool.gcc.dependency.asm = $(call sm.tool.gcc.dependency,asm,$1,$2,$3,$4)
+
 
 ##################################################
 # Links
@@ -118,6 +121,10 @@ define sm.tool.gcc.link
 $(sm.tool.gcc.cmd.ld) $($(strip $3)) -o $(strip $1) $(strip $2) $($(strip $4))
 endef
 
+
+##################################################
+# Archive
+
 define sm.tool.gcc.archive
 $(sm.tool.gcc.cmd.ar) $(strip $1) $(strip $2)
 endef
@@ -125,6 +132,10 @@ endef
 sm.tool.gcc.archive.c   = $(call sm.tool.gcc.archive,$1,$2,$3,$4)
 sm.tool.gcc.archive.c++ = $(call sm.tool.gcc.archive,$1,$2,$3,$4)
 sm.tool.gcc.archive.asm = $(call sm.tool.gcc.archive,$1,$2,$3,$4)
+
+
+######################################################################
+# Options
 
 ifeq ($(strip $(sm.config.variant)),debug)
   sm.tool.gcc.compile.options := -g -ggdb
