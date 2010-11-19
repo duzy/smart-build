9c638d14e3e37e1983e5472ca59c74c069c191af
Duzy Chan <duzy@duzy.info>
2010-11-19 11:47:58 +0800

    fix: compilation of examples/subdirs

--- examples/subdirs/foo/smart.mk
+++ examples/subdirs/foo/smart.mk
@@ -5,6 +5,12 @@ $(call sm-new-module, foo, shared)
 sm.this.verbose := true
 sm.this.toolset := gcc
 sm.this.sources := foo.cpp
-sm.this.link.options := -Wl,--out-implib,$(sm.out.lib)/libfoo.a
+ifeq ($(sm.os.name),win32)
+  sm.this.link.options := -Wl,--out-implib,$(sm.out.lib)/libfoo.a
+else
+  sm.this.depends += $(sm.out.lib)/libfoo.so
+  $(sm.out.lib)/libfoo.so : $(sm.out.lib) $(sm.out.bin)/foo.so
+	$(call sm.tool.common.ln,$(sm.this.dir)/$(sm.out.bin)/foo.so,$@)
+endif
 
 $(sm-build-this)
