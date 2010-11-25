067b8e609f839528a2b617599663551fec79ea12
Duzy Chan <code@duzy.info>
2010-11-25 10:40:10 +0800

    move callbacks from tools/gcc.mk into rules.mk

--- rules.mk
+++ rules.mk
@@ -2,8 +2,8 @@
 #	Copyright(c) 2010,2009, by Zhan Xin-ming, duzy@duzy.info
 #
 
-## $(call sm.rule.template, ACTION, LANG, TARGET, PREREQUISITES, sources-replacement, callback, callback)
-define sm.rule.template
+## eg. $(call sm.code.rule, ACTION, LANG, TARGET, PREREQUISITES, sources-replacement, callback, callback)
+define sm.code.rule
 $(if $(sm.this.toolset),,$(error smart: 'sm.this.toolset' must be set for '$2'))\
 $(if $(sm.tool.$(sm.this.toolset).$1.$2),,$(error smart: 'sm.tool.$(sm.this.toolset).$1.$2' not set))\
 $(call sm-check-flavor,sm.tool.$(sm.this.toolset).$1.$2,recursive,\
@@ -13,11 +13,10 @@ $(call sm-check-flavor,sm.tool.$(sm.this.toolset).$1.$2,recursive,\
 	$(if $(call equal,$(sm.this.verbose),true),,$(if $(call equal,$1,compile),\
                         $$(info $2: $(sm.this.name) += $(4:$(sm.top)/%=%)),\
                         $$(info $2: $(sm.this.name) -> $3)\
-             )@)$$(call sm.tool.$(sm.this.toolset).$1.$2,$$@,$(if $5,$5,$4),$6,$7)
-endef #sm.rule.template
+             )@)echo $$(call sm.tool.$(sm.this.toolset).$1.$2,$$@,$(if $5,$5,$4),$$($6),$$($7))
+endef #sm.code.rule
 
 ## eg. $(call sm.rule, ACTION, LANG, TARGET, PREREQUISITES, callback-FLAGS [,callack-LIBS])
-#
 define sm.rule
 $(if $1,,$(error smart: 'sm.rule': arg 1 must be the action (eg. compile, link)))\
 $(if $2,,$(error smart: 'sm.rule': arg 2 must be the language type))\
@@ -26,7 +25,7 @@ $(if $4,,$(error smart: 'sm.rule': arg 4 must be the source file))\
 $(if $5,$(if $(call equal,$(5:@%=@),@),,$(error smart: 'sm.rule': arg 5 must be the sources as in '@file')))\
 $(if $6,,$(error smart: 'sm.rule': arg 6 must be a callback for command line flags))\
 $(if $7,$(call sm-check-defined,$7,smart: 'sm.rule': arg 7 must be a callback for command line flags))\
-$(eval $(call sm.rule.template,$(strip $1),$(strip $2),$(strip $3),$(strip $4),$(strip $5),$(strip $6),$(strip $7)))
+$(eval $(call sm.code.rule,$(strip $1),$(strip $2),$(strip $3),$(strip $4),$(strip $5),$(strip $6),$(strip $7)))
 endef #sm.rule
 
 ## eg. $(call sm.rule.dependency, LANG, TARGET, PREREQUISITES, callback-FLAGS)
@@ -74,4 +73,3 @@ $(if $4,$(if $(call equal,$(4:@%=@),@),,$(error smart: 'sm.rule.archive': arg 4 
 $(if $5,$(call sm-check-defined,$5,smart: 'sm.rule.archive': '$5' must be defined as a callback for archive flags))\
 $(call sm.rule,archive,$(strip $1),$(strip $2),$(strip $3),$(strip $4),$(strip $5))
 endef #sm.rule.archive
-
