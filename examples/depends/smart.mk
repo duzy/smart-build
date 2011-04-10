ab863bbe8e20c1b8b6acf3fd2c0f523778e64aec
Duzy Chan <code@duzy.info>
2011-04-10 18:18:01 +0800

    add examples/depends

---
+++ examples/depends/smart.mk
@@ -0,0 +1,20 @@
+#
+
+$(call sm-new-module, foo, shared)
+
+sm.this.verbose := true
+sm.this.toolset := gcc
+sm.this.sources := foo.cpp
+ifeq ($(sm.os.name),win32)
+  sm.this.link.options := -Wl,--out-implib,$(sm.out.lib)/libfoo.a
+else
+  sm.this.depends += $(sm.out.lib)/libfoo.so
+  $(sm.out.lib)/libfoo.so : $(sm.out.lib) $(sm.out.bin)/foo.so
+	$(call sm.tool.common.ln,$(sm.this.dir)/$(sm.out.bin)/foo.so,$@)
+endif
+
+sm.this.depends := foo.txt
+
+foo.txt: ; echo foo > $@
+
+$(sm-build-this)
