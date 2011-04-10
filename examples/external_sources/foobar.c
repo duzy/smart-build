2f42fb34968b7325373cf3d44fbd0c9842fc8461
Duzy Chan <code@duzy.info>
2011-04-10 18:03:42 +0800

    add examples/external_sources

---
+++ examples/external_sources/foobar.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+int main() { printf("smart::build::example::module: %s\n", TEST); return 0; }
--- examples/external_sources/smart.mk
+++ examples/external_sources/smart.mk
@@ -1,5 +1,7 @@
 #
 
+toolset ?= gcc
+
 $(call sm-new-module, foo, executable)
 $(call sm-check-not-empty,sm.this.dir)
 $(call sm-check-not-empty,sm.this.type)
@@ -11,37 +13,22 @@ $(call sm-check-equal,$(sm.this.name),foo)
 $(call sm-check-equal,$(sm.this.type),exe)
 $(call sm-check-equal,$(sm.this.suffix),$(if $(sm.os.name.win32),.exe))
 
-## Turn on verbose to make command lines visible
+## Turn off verbose to make command lines invisible
 sm.this.verbose := false
 
 ## Choose a toolset (doing this will enable tools/$(sm.this.toolset).mk),
 ## if not doing this, the old style of build system will be used (which only
 ## supports gcc toolset).
-sm.this.toolset := gcc
+sm.this.toolset := $(toolset)
 
 ## The flags to be used by the compiler
 sm.this.compile.flags := -DTEST=\"$(sm.this.name)\"
 
-## The include search path (for compiler's -I switch), each item of this will
-## be translated into a -I switch for the compiler by the toolset.
-sm.this.includes := $(sm.this.dir)/../include
-sm.this.sources := foobar.c
+## External sources is NOT related to $(sm.this.dir)
+sm.this.sources.external := $(sm.this.dir)/foobar.c
 
 ## The flags to be used by the linker
-## NOTE: no needs to add '-Wl,' or '-Wlinker' to pass linker arguments
+## NOTE: no needs to '-Wl,' or '-Wlinker' to pass linker arguments
 sm.this.link.flags := $(if $(sm.os.name.win32),--subsystem=console)
 
-## The libraries search path (for linker's -L switch), each item of this will
-## be translated into a -L switch similar to 'sm.this.includes'.
-sm.this.libdirs := $(sm.this.dir)/../libs
-
-## The libraries to be linked with this module, each item of which will be
-## translated into a -l switch in this way:
-##	libNAME	-> -lNAME
-##	NAME	-> -lNAME
-##	-lNAME	-> -lNAME
-sm.this.libs := 
-
-$(call sm-copy-files, foobar.txt, $(sm.out))
-$(call sm-copy-headers, foobar.h, foo)
 $(sm-build-this)
