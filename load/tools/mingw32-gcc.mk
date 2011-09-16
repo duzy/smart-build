aad80d6bd34e78a8b398360edf7a22b2fbdae218
Duzy Chan <code@duzy.info>
2011-09-16 17:40:06 +0800

    fix tools/mingw32-gcc

--- load/tools/mingw32-gcc.mk
+++ load/tools/mingw32-gcc.mk
@@ -11,7 +11,7 @@
 ## make sure that gcc.mk is included only once
 $(call sm-check-origin, sm.tool.mingw32-gcc, undefined)
 
-NULL :=
+null :=
 
 TOOLSET_PRE := i586-mingw32msvc-
 
@@ -46,110 +46,71 @@ sm.tool.mingw32-gcc.target.suffix.linux.depends :=
 ######################################################################
 # Compiles
 
-##
-##  Produce compile commands for c language
-##
 define sm.tool.mingw32-gcc.compile.c.private
-$(sm.tool.mingw32-gcc.cmd.c) $(strip $3) -c -o $(strip $1) $(strip $2)
+$(sm.tool.mingw32-gcc.cmd.c) $(sm.args.flags.0) -c -o $(sm.args.target) $(sm.args.sources)
 endef #sm.tool.mingw32-gcc.compile.c.private
 
-##
-##
-##
 define sm.tool.mingw32-gcc.compile.c++.private
-$(sm.tool.mingw32-gcc.cmd.c++) $(strip $3) -c -o $(strip $1) $(strip $2)
+$(sm.tool.mingw32-gcc.cmd.c++) $(sm.args.flags.0) -c -o $(sm.args.target) $(sm.args.sources)
 endef #sm.tool.mingw32-gcc.compile.c++.private
 
-##
-##
-##
 define sm.tool.mingw32-gcc.compile.asm.private
-$(sm.tool.mingw32-gcc.cmd.asm) $(strip $3) -c -o $(strip $1) $(strip $2)
+$(sm.tool.mingw32-gcc.cmd.asm) $(sm.args.flags.0) -c -o $(sm.args.target) $(sm.args.sources)
 endef #sm.tool.mingw32-gcc.compile.asm.private
 
-##
-##
-##
-define sm.tool.mingw32-gcc.compile
-$(if $1,,$(error smart: arg 1 must be the source language))\
-$(if $2,,$(error smart: arg 2 must be the output target))\
-$(if $3,,$(error smart: arg 3 must be the source file))\
-$(if $4,,$(error smart: arg 4 must be compile flags))\
-$(call sm.tool.mingw32-gcc.compile.$(strip $1).private,$2,$3,$(strip $4))
-endef #sm.tool.mingw32-gcc.compile
-
-sm.tool.mingw32-gcc.compile.c = $(call sm.tool.mingw32-gcc.compile,c,$1,$2,$3)
-sm.tool.mingw32-gcc.compile.c++ = $(call sm.tool.mingw32-gcc.compile,c++,$1,$2,$3)
-sm.tool.mingw32-gcc.compile.asm = $(call sm.tool.mingw32-gcc.compile,asm,$1,$2,$3)
+sm.tool.mingw32-gcc.compile     = $(sm.tool.mingw32-gcc.compile.private)
+sm.tool.mingw32-gcc.compile.c   = $(eval sm.args.lang:=c)$(sm.tool.mingw32-gcc.compile)
+sm.tool.mingw32-gcc.compile.c++ = $(eval sm.args.lang:=c++)$(sm.tool.mingw32-gcc.compile)
+sm.tool.mingw32-gcc.compile.asm = $(eval sm.args.lang:=asm)$(sm.tool.mingw32-gcc.compile)
 
 
 ##################################################
 # Denpendencies
 
 define sm.tool.mingw32-gcc.dependency.c.private
-$(sm.tool.mingw32-gcc.cmd.c) -MM -MT $(strip $2) -MF $(strip $1) $(strip $4) $(strip $3)
+$(sm.tool.mingw32-gcc.cmd.c) -MM -MT $(sm.args.target) -MF $(sm.args.output) $(sm.args.flags.0) $(sm.args.sources)
 endef #sm.tool.mingw32-gcc.dependency.c.private
 
 define sm.tool.mingw32-gcc.dependency.c++.private
-$(sm.tool.mingw32-gcc.cmd.c++) -MM -MT $(strip $2) -MF $(strip $1) $(strip $4) $(strip $3)
+$(sm.tool.mingw32-gcc.cmd.c++) -MM -MT $(sm.args.target) -MF $(sm.args.output) $(sm.args.flags.0) $(sm.args.sources)
 endef #sm.tool.mingw32-gcc.dependency.c++.private
 
-## eg. $(sm.tool.mingw32-gcc.dependency, foo.d, foo.o, src/foo.cpp)
-define sm.tool.mingw32-gcc.dependency
-$(if $1,,$(error smart: arg 1 must be the source language))\
-$(if $2,,$(error smart: arg 2 must be the output target))\
-$(if $3,,$(error smart: arg 3 must be the source file))\
-$(if $4,,$(error smart: arg 4 must be compile flags))\
-$(call sm.tool.mingw32-gcc.dependency.$(strip $1).private,$2,$3,$(strip $4),$(strip $5))
-endef #sm.tool.mingw32-gcc.dependency
-
-sm.tool.mingw32-gcc.dependency.c = $(call sm.tool.mingw32-gcc.dependency,c,$1,$2,$3,$4)
-sm.tool.mingw32-gcc.dependency.c++ = $(call sm.tool.mingw32-gcc.dependency,c++,$1,$2,$3,$4)
-sm.tool.mingw32-gcc.dependency.asm = $(call sm.tool.mingw32-gcc.dependency,asm,$1,$2,$3,$4)
+sm.tool.mingw32-gcc.dependency     = $(call sm.tool.mingw32-gcc.dependency.private)
+sm.tool.mingw32-gcc.dependency.c   = $(eval sm.args.lang:=c)$(call sm.tool.mingw32-gcc.dependency)
+sm.tool.mingw32-gcc.dependency.c++ = $(eval sm.args.lang:=c++)$(call sm.tool.mingw32-gcc.dependency)
+sm.tool.mingw32-gcc.dependency.asm = $(eval sm.args.lang:=asm)$(call sm.tool.mingw32-gcc.dependency)
 
 
 ##################################################
 # Links
 
-##
-##
-##
 define sm.tool.mingw32-gcc.link.c
-$(sm.tool.mingw32-gcc.cmd.c) $(strip $3) -o $(strip $1) $(strip $2) $(strip $4)
+$(sm.tool.mingw32-gcc.cmd.c) $(sm.args.flags.0) -o $(sm.args.target) $(sm.args.sources) $(sm.args.flags.1)
 endef #sm.tool.mingw32-gcc.link.c
 
-##
-##
-##
 define sm.tool.mingw32-gcc.link.c++
-$(sm.tool.mingw32-gcc.cmd.c++) $(strip $3) -o $(strip $1) $(strip $2) $(strip $4)
+$(sm.tool.mingw32-gcc.cmd.c++) $(sm.args.flags.0) -o $(sm.args.target) $(sm.args.sources) $(sm.args.flags.1)
 endef #sm.tool.mingw32-gcc.link.c++
 
-##
-##
-##
 define sm.tool.mingw32-gcc.link.asm
-$(sm.tool.mingw32-gcc.cmd.as) $(strip $3) -o $(strip $1) $(strip $2) $(strip $4)
+$(sm.tool.mingw32-gcc.cmd.as) $(sm.args.flags.0) -o $(sm.args.target) $(sm.args.sources) $(sm.args.flags.1)
 endef #sm.tool.mingw32-gcc.link.asm
 
-##
-##
-## eg. $(call sm.tool.mingw32-gcc.link, foo, foo.c, options, libs)
 define sm.tool.mingw32-gcc.link
-$(sm.tool.mingw32-gcc.cmd.ld) $(strip $3) -o $(strip $1) $(strip $2) $(strip $4)
-endef
+$(sm.tool.mingw32-gcc.cmd.ld) $(sm.args.flags.0) -o $(sm.args.target) $(sm.args.sources) $(sm.args.flags.1)
+endef #sm.tool.mingw32-gcc.link
 
 
 ##################################################
 # Archive
 
 define sm.tool.mingw32-gcc.archive
-$(sm.tool.mingw32-gcc.cmd.ar) $(strip $1) $(strip $2)
-endef
+$(sm.tool.mingw32-gcc.cmd.ar) $(sm.args.target) $(sm.args.sources)
+endef #sm.tool.mingw32-gcc.archive
 
-sm.tool.mingw32-gcc.archive.c   = $(call sm.tool.mingw32-gcc.archive,$1,$2,$3,$4)
-sm.tool.mingw32-gcc.archive.c++ = $(call sm.tool.mingw32-gcc.archive,$1,$2,$3,$4)
-sm.tool.mingw32-gcc.archive.asm = $(call sm.tool.mingw32-gcc.archive,$1,$2,$3,$4)
+sm.tool.mingw32-gcc.archive.c   = $(sm.tool.mingw32-gcc.archive)
+sm.tool.mingw32-gcc.archive.c++ = $(sm.tool.mingw32-gcc.archive)
+sm.tool.mingw32-gcc.archive.asm = $(sm.tool.mingw32-gcc.archive)
 
 
 ######################################################################
@@ -172,7 +133,7 @@ ifeq ($(sm.os.name),win32)
   sm.tool.mingw32-gcc.link.options += -mwindows \
     -Wl,--enable-runtime-pseudo-reloc \
     -Wl,--enable-auto-import \
-    $(NULL)
+    $(null)
 endif#win32
 endif#linux
 
