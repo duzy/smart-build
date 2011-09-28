5f1c0b015f90691e2096004873497788637dbb8b
Duzy Chan <code@duzy.info>
2011-09-28 11:15:11 +0800

    cleanup sm.this.sources.LANG accordint sm.var.common.langs.extra in postload

--- load/postload.mk
+++ load/postload.mk
@@ -4,3 +4,6 @@
 
 ## This file is expected to be included AFTER including an 'smart.mk'(module).
 
+$(foreach sm.var.temp._lang,$(sm.var.common.langs.extra),\
+   $(eval sm.this.sources.$(sm.var.temp._lang) :=)\
+   $(eval sm.this.sources.has.$(sm.var.temp._lang) :=))
