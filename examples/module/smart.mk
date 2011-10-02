76382bd46cae8409dbcdcf8a9304e4c8580ef4e7
Duzy Chan <code@duzy.info>
2011-10-03 01:48:40 +0800

    cleanup out dated comments

--- examples/module/smart.mk
+++ examples/module/smart.mk
@@ -8,34 +8,16 @@ $(call sm-check-not-empty,sm.this.name)
 $(call sm-check-not-empty,sm.this.makefile)
 $(call sm-check-in-list,foo,sm.global.modules)
 $(call sm-check-equal,$(sm.this.name),foo)
-$(call sm-check-equal,$(sm.this.type),exe,gcc)
+$(call sm-check-equal,$(sm.this.type),exe)
+$(call sm-check-equal,$(sm.this.toolset),gcc)
 $(call sm-check-equal,$(sm.this.suffix),$(if $(sm.os.name.win32),.exe))
 
-## Turn on verbose to make command lines visible
 sm.this.verbose := false
-
-## The flags to be used by the compiler
-sm.this.compile.flags := -DTEST=\"$(sm.this.name)\"
-
-## The include search path (for compiler's -I switch), each item of this will
-## be translated into a -I switch for the compiler by the toolset.
-sm.this.includes := $(sm.this.dir)/../include
 sm.this.sources := foobar.c
-
-## The flags to be used by the linker
-## NOTE: no needs to '-Wl,' or '-Wlinker' to pass linker arguments
+sm.this.compile.flags := -DTEST=\"$(sm.this.name)\"
+sm.this.includes := $(sm.this.dir)/../include
 sm.this.link.flags := $(if $(sm.os.name.win32),--subsystem=console)
-
-## The libraries search path (for linker's -L switch), each item of this will
-## be translated into a -L switch similar to 'sm.this.includes'.
 sm.this.libdirs := $(sm.this.dir)/../libs
-
-## The libraries to be linked with this module, each item of which will be
-## translated into a -l switch in this way:
-##	libNAME	-> -lNAME
-##	NAME	-> -lNAME
-##	-lNAME	-> -lNAME
 sm.this.libs := 
 
 $(sm-build-this)
-#$(call sm-check-equal,$(sm.this.suffix),$(if $(sm.os.name.win32),.exe))
