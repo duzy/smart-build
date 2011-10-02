1086e329a2870a1051c60772065a0675c2c5d067
Duzy Chan <code@duzy.info>
2011-10-02 17:43:08 +0800

    add more check points in examples

--- examples/cweb-foo/smart.mk
+++ examples/cweb-foo/smart.mk
@@ -8,6 +8,15 @@ sm.this.sources := main.w
 $(sm-build-this)
 
 prefix := $(sm.out.inter)/common
-$(call sm-check-equal,$(strip $(sm.var.foo.sources.c++)),$(prefix)/main.cpp)
+$(call sm-check-equal,$(sm.module.foo.sources.unknown),)
+$(call sm-check-equal,$(sm.module.foo.sources.common),main.w)
+$(call sm-check-equal,$(sm.module.foo.sources.cweb),main.w)
+$(call sm-check-equal,$(sm.module.foo.sources.has.cweb),true)
+$(call sm-check-equal,$(sm.module.foo.sources.c++),$(prefix)/main.cpp)
+$(call sm-check-equal,$(sm.module.foo.sources.has.c++),true)
+$(call sm-check-equal,$(sm.module.foo.intermediates),$(sm.out.inter)/$(prefix)/main.o)
+$(call sm-check-equal,$(sm.module.foo.targets),$(sm.out.bin)/foo)
+$(call sm-check-equal,$(sm.module.foo.module_targets),$(sm.out.bin)/foo)
+$(call sm-check-equal,$(sm.module.foo.user_defined_targets),)
 $(call sm-check-equal,$(strip $(sm.this.sources.c++)),$(prefix)/main.cpp)
 $(call sm-check-equal,$(strip $(sm.this.inters)),$(sm.out.inter)/$(prefix)/main.o)
