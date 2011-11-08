6a7a90250dbf18ffa60027359073f2453f4614fe
Duzy Chan <code@duzy.info>
2011-11-08 10:13:14 +0800

    add some more test cases

--- test/module-of-type-exe-use-static.mk
+++ test/module-of-type-exe-use-static.mk
@@ -9,6 +9,7 @@ $(call test-check-defined, sm.this.dir)
 $(call test-check-value-pat-of,sm.this.dir,%/test)
 $(call test-check-value-pat-of,sm.this.makefile,%/test/module-of-type-exe-use-static.mk)
 #$(call test-check-value-of,sm.this.suffix,)
+$(call test-check-value-of,sm.this.toolset,gcc)
 
 $(call test-check-value-of,sm.module.module-of-type-static.name,module-of-type-static)
 $(call test-check-value-of,sm.module.module-of-type-static.export.libs,module-of-type-static)
@@ -20,5 +21,6 @@ $(call test-check-value-of,sm.this.using_list,module-of-type-static)
 
 sm.this.sources := main.c
 
+$(call test-check-undefined,sm.module.module-of-type-exe-use-static.used.libs)
 $(sm-build-this)
 $(call test-check-value,$(strip $(sm.module.module-of-type-exe-use-static.used.libs)),module-of-type-static)
