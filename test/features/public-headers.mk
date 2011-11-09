d7bc74d175199e0399c315c3c8b90cf1d99c1922
Duzy Chan <code@duzy.info>
2011-11-09 14:30:56 +0800

    enhanced feature test "public-headers"

--- test/features/public-headers.mk
+++ test/features/public-headers.mk
@@ -1,8 +1,16 @@
 #
 #
 $(call test-check-undefined, sm.this.dir)
+$(foreach _, $(filter sm.this.headers.%,$(.VARIABLES)),\
+             $(call test-check-undefined,$_))
 $(call sm-new-module, feature-public-headers, depends)
 
 sm.this.headers.test/features := foo.h bar.h
 
-$(sm-build-this)
+$(call test-check-undefined,sm.module.feature-public-headers.name)
+$(call test-check-undefined,sm.module.feature-public-headers.type)
+$(call test-check-undefined,sm.module.feature-public-headers.headers.test/features)
+$(sm-build-this) ## make rules
+$(call test-check-value-of,sm.module.feature-public-headers.name,feature-public-headers)
+$(call test-check-value-of,sm.module.feature-public-headers.type,depends)
+$(call test-check-value-of,sm.module.feature-public-headers.headers.test/features,foo.h bar.h)
