ebd635b068118e67ff085bcc47385924f942f007
Duzy Chan <code@duzy.info>
2011-11-09 13:10:52 +0800

    prompt executable, static-library, shared-library, test for targets

--- load/rules.mk
+++ load/rules.mk
@@ -2,6 +2,14 @@
 #	Copyright(c) 2010,2009, by Zhan Xin-ming, duzy@duzy.info
 #
 
+sm.var.target_type.static := static-library
+sm.var.target_type.shared := shared-library
+sm.var.target_type.exe := executable
+sm.var.target_type.t := test
+define sm.fun.get-target-type
+$(strip $(or $(sm.var.target_type.$($(sm._this).type)),$($(sm._this).type)))
+endef #sm.fun.get-target-type
+
 define sm-rule
 $(call sm-check-flavor,\
    $(sm.var.toolset).$(sm.args.action).$(sm.args.lang),recursive,\
@@ -21,7 +29,7 @@ $(eval \
       ifeq ($(sm.args.action),compile)
 	$$(info $(sm.args.lang): $($(sm._this).name) += $(sm.args.sources:$(sm.top)/%=%))
       else
-	$$(info $(sm.args.lang): $($(sm._this).name) -> $(sm.args.target))
+	$$(info $(sm.fun.get-target-type): $($(sm._this).name) -> $(sm.args.target))
       endif
 	$(sm.var.Q)$(filter %, $($(sm.var.toolset).$(sm.args.action).$(sm.args.lang)))
     endif
