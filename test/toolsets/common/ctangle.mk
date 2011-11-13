f87992eedc15090c0e3c7b5b6bb161f424ef5f8e
Duzy Chan <code@duzy.info>
2011-11-13 09:44:16 +0800

    Revert "take off common toolset"
    This reverts commit 6b2fefc9c44eab99776e602bd86a346504282ca5.
    
    Since we are on the 'common' branch

---
+++ test/toolsets/common/ctangle.mk
@@ -0,0 +1,24 @@
+#
+
+$(call sm-new-module, toolset-common-ctangle, exe, gcc)
+
+sm.this.lang := c
+sm.this.langs := c TeX
+sm.this.sources := ctangle.w common.w cwebman.tex
+
+$(sm-build-this)
+
+# prefix := $(sm.out.inter)/common
+# $(call sm-check-equal,$(sm.module.ctangle.sources.unknown),)
+# $(call sm-check-equal,$(sm.module.ctangle.sources.common),ctangle.w common.w cwebman.tex)
+# $(call sm-check-equal,$(sm.module.ctangle.sources.c),$(prefix)/ctangle.c $(prefix)/common.c)
+# $(call sm-check-equal,$(sm.module.ctangle.sources.TeX),cwebman.tex $(prefix)/ctangle.tex $(prefix)/common.tex)
+# $(call sm-check-equal,$(sm.module.ctangle.sources.has.c),true)
+# $(call sm-check-equal,$(sm.module.ctangle.sources.has.TeX),true)
+# $(call sm-check-equal,$(sm.module.ctangle.intermediates),$(sm.out.inter)/$(prefix)/ctangle.o $(sm.out.inter)/$(prefix)/common.o)
+# $(call sm-check-equal,$(sm.module.ctangle.targets),$(sm.out.bin)/ctangle)
+# $(call sm-check-equal,$(sm.this.sources.c),$(prefix)/ctangle.c $(prefix)/common.c)
+# $(call sm-check-equal,$(sm.this.sources.TeX),cwebman.tex $(prefix)/ctangle.tex $(prefix)/common.tex)
+# $(call sm-check-equal,$(sm.this.intermediates),$(sm.out.inter)/$(prefix)/ctangle.o $(sm.out.inter)/$(prefix)/common.o)
+# $(call sm-check-equal,$(sm.this.sources.has.c),true)
+# #$(call sm-check-equal,$(sm.this.sources.has.TeX),true)
