7e7b51322e84cad97d2a284ae96dc06a5441674f
Duzy Chan <code@duzy.info>
2011-11-14 20:09:54 +0800

    remove a failed test case

--- test/features/flags-in-file-compile-post.sh
+++ test/features/flags-in-file-compile-post.sh
@@ -4,5 +4,5 @@ test-check-file ${BASH_SOURCE}:${LINENO} $OUT_INTERS/features/feature-flags-in-f
 test-check-file ${BASH_SOURCE}:${LINENO} $OUT_TEMP/feature-flags-in-file-compile/compile.flags.0.c
 
 out=`cat $OUT_TEMP/feature-flags-in-file-compile/compile.flags.0.c`
-test-check-value-contains ${BASH_SOURCE}:${LINENO} "$out" "-DTEST=\\\"feature-flags-in-file-compile\\\""
+#test-check-value-contains ${BASH_SOURCE}:${LINENO} "$out" "-DTEST=\\\"feature-flags-in-file-compile\\\""
 test-check-value ${BASH_SOURCE}:${LINENO} "$out" "-g -ggdb -DTEST=\\\"feature-flags-in-file-compile\\\""
