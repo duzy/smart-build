96a00b4f15c3bbf6607b7d2d124cdfe0d840b4e5
Duzy Chan <code@duzy.info>
2011-12-21 10:24:13 +0800

    test: use test-check-module-empty

--- test/features/copy-files.mk
+++ test/features/copy-files.mk
@@ -1,6 +1,7 @@
 #
 #
 $(call test-check-undefined, sm.this.dir)
+$(call test-check-module-empty, sm.this)
 $(call sm-new-module, feature-copy-files, none: depends)
 
 $(call sm-copy-files, foo.txt, $(sm.top)/out)
