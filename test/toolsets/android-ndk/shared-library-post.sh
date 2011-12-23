0d0d4a5c040df41ee7394fe27d2dad9a6cf8fc42
Duzy Chan <code@duzy.info>
2011-12-23 12:05:03 +0800

    android-ndk: updates shared-library test case

--- test/toolsets/android-ndk/shared-library-post.sh
+++ test/toolsets/android-ndk/shared-library-post.sh
@@ -7,6 +7,8 @@ test-check-file ${BASH_SOURCE}:${LINENO} $TOP/out/android-ndk/debug/lib/libandro
 out=`file $TOP/out/android-ndk/debug/bin/native-activity.so`
 test-check-value-contains ${BASH_SOURCE}:${LINENO} "$out" "ARM"
 
-make -f $TOP/main.mk ndk-libs
-
-test-check-file ${BASH_SOURCE}:${LINENO} $TOP/libs/armeabi/libnative-activity.so
+make -f $TOP/main.mk ndk-libs && {
+    test-check-file ${BASH_SOURCE}:${LINENO} $TOP/libs/armeabi/libnative-activity.so
+} || {
+    echo ${BASH_SOURCE}:${LINENO} "failed 'make ndk-libs'"
+}
