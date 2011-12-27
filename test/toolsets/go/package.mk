cd67e84f6f4770632e31fa38f0f9d004f3fe3995
Duzy Chan <code@duzy.info>
2011-12-27 18:58:00 +0800

    update test cases

--- test/toolsets/go/package.mk
+++ test/toolsets/go/package.mk
@@ -5,7 +5,5 @@ $(call test-check-module-empty, sm.module.toolset_go_package)
 $(call sm-new-module, toolset_go_package, go: package)
 
 sm.this.sources := foo.go foo.c
-sm.this.export.compile.flags := -I$(sm.out.lib)
-sm.this.export.link.flags := -L$(sm.out.lib)
 
 $(sm-build-this)
