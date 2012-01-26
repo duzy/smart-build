db3667d499ac1c7362bfbf39309bfcd72513f6f3
Duzy Chan <code@duzy.info>
2012-01-27 00:54:52 +0800

    fixed test case per-source-compile-flags

--- test/toolsets/gcc/features/per-source-compile-flags/smart.mk
+++ test/toolsets/gcc/features/per-source-compile-flags/smart.mk
@@ -5,8 +5,8 @@ $(call sm-new-module, feature-per-source-compile-flags, gcc: exe)
 
 sm.this.sources := ../foo.c ../bar.c ../foobar.c
 sm.this.compile.flags += -g
-sm.this.compile.flags-foo.c := -DTEST=\"foo\"
-sm.this.compile.flags-bar.c := -DTEST=\"bar\"
+sm.this.compile.flags-../foo.c := -DTEST=\"foo\"
+sm.this.compile.flags-../bar.c := -DTEST=\"bar\"
 sm.this.link.flags += -Dfoobar
 
 $(sm-build-this)
