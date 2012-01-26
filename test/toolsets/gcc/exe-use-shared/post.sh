59b3a6a9fb6b4ed4203567f0a9d5dafaf1861056
Duzy Chan <code@duzy.info>
2012-01-27 00:01:38 +0800

    fixed test case exe-use-shared

--- test/toolsets/gcc/exe-use-shared/post.sh
+++ test/toolsets/gcc/exe-use-shared/post.sh
@@ -1,12 +1,12 @@
 # -*- bash -*-
 test-check-file ${BASH_SOURCE}:${LINENO} out/gcc/debug/intermediates/exe-use-shared/_/main.c.o
 test-check-file ${BASH_SOURCE}:${LINENO} out/gcc/debug/intermediates/exe-use-shared/_/main.c.o.d
-test-check-file ${BASH_SOURCE}:${LINENO} out/gcc/debug/intermediates/gcc-shared/_/bar.c.o
-test-check-file ${BASH_SOURCE}:${LINENO} out/gcc/debug/intermediates/gcc-shared/_/bar.c.o.d
-test-check-file ${BASH_SOURCE}:${LINENO} out/gcc/debug/intermediates/gcc-shared/_/foo.c.o
-test-check-file ${BASH_SOURCE}:${LINENO} out/gcc/debug/intermediates/gcc-shared/_/foo.c.o.d
-test-check-file ${BASH_SOURCE}:${LINENO} out/gcc/debug/intermediates/gcc-shared/_/foo.go.o
-test-check-file ${BASH_SOURCE}:${LINENO} out/gcc/debug/intermediates/gcc-shared/_/foo.go.o.d
+test-check-file ${BASH_SOURCE}:${LINENO} out/gcc/debug/intermediates/gcc-shared/_/shared/_/bar.c.o
+test-check-file ${BASH_SOURCE}:${LINENO} out/gcc/debug/intermediates/gcc-shared/_/shared/_/bar.c.o.d
+test-check-file ${BASH_SOURCE}:${LINENO} out/gcc/debug/intermediates/gcc-shared/_/shared/_/foo.c.o
+test-check-file ${BASH_SOURCE}:${LINENO} out/gcc/debug/intermediates/gcc-shared/_/shared/_/foo.c.o.d
+test-check-file ${BASH_SOURCE}:${LINENO} out/gcc/debug/intermediates/gcc-shared/_/shared/_/foo.go.o
+test-check-file ${BASH_SOURCE}:${LINENO} out/gcc/debug/intermediates/gcc-shared/_/shared/_/foo.go.o.d
 test-check-file ${BASH_SOURCE}:${LINENO} out/gcc/debug/bin/gcc-shared.so
 test-check-file ${BASH_SOURCE}:${LINENO} out/gcc/debug/lib/libgcc-shared.so
 test-check-file ${BASH_SOURCE}:${LINENO} out/gcc/debug/bin/exe-use-shared$EXE
